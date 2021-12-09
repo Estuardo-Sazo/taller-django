d3cba6b961d2579ec2a12ff43d593a17f240e6fb
roberto-2201 <83876429+roberto-2201@users.noreply.github.com>
2021-12-09 17:53:10 -0600

    Update models.py

--- clientes/models.py
+++ clientes/models.py
@@ -8,6 +8,7 @@ class Cliente(models.Model):
     correo=models.EmailField(max_length=100, verbose_name='Correo', unique=True)
     telefono=models.CharField(max_length=100, verbose_name='Telefono', null=True)
 
+    # campos para guardar el  la cracion  o modificacion de fecha
     created= models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     
