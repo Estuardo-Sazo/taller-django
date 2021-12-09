c33215e5959dfc691093d82ef1f16508973da3a5
roberto-2201 <83876429+roberto-2201@users.noreply.github.com>
2021-12-09 17:54:58 -0600

    Update models.py

--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# # crar  modelos  para formularios  tipo charfile
 class Cliente(models.Model):
     id=models.AutoField(primary_key=True)
     nombre=models.CharField(max_length=100, verbose_name='Nombre', null=False)
